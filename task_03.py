666a8aae3e74fc327e3b7908071ed5f754641e49
Michael Meyers <mjmeyer2013@gmail.com>
2015-09-08 01:32:09 -0400

    task 3 assignment 2 week 2

--- task_03.py
+++ task_03.py
@@ -1,4 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Error removed from task 03"""
+
+CHARLIE = "Brown" 
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
